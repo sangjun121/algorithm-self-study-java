17425ba58467f4368cf6a67f78d457558cd3b516
조상준 <juncho12011201@gmail.com>
2026-06-21 19:13:49 +0900

    [프로그래머스 42576] 완주하지 못한 선수 문제풀이

---
+++ soving/src/main/java/me/sangjun/hashmap/Pg42576.java
@@ -0,0 +1,18 @@
+package me.sangjun.hashmap;
+import java.util.*;
+
+public class Pg42576 {
+    public String solution(String[] participant, String[] completion) {
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i = 0; i < completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+
+        return participant[participant.length-1];
+    }
+}
